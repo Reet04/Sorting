92f3804158aa67b573c2476fea382d04e87d75e6
Reet04 <reetagg111@gmail.com>
2021-02-01 09:57:08 +0530

    Bubble Sort

---
+++ src/BubbleSort.java
@@ -0,0 +1,26 @@
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]= {2,5,7,4,16,23,11,4};
+		for(int i=0;i<7;i++)
+		{
+			for(int j=0;j<7-i;j++)
+			{
+				if(arr[j]>arr[j+1])
+				{
+					int temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}
+			}
+		}
+		for(int i=0;i<8;i++)
+			System.out.println(arr[i]);
+		
+		
+		
+
+	}
+
+}
